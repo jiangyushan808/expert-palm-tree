94d513c0d7d9673e47a63a9c334643689a9029c8
jys <jys@qq.com>
2025-07-17 00:31:49 +0800

    no wu cha

---
+++ MDK-ARM/motor.h
@@ -0,0 +1,22 @@
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+#include "stm32f4xx_hal.h" //HAL?????
+#include <main.h>
+//PWM限幅 宏定义
+#define PWM_MAX  999  // TIM1的Period=999
+#define PWM_MIN -999
+#define Dead_Voltage  100	//电机死区电压 PMW = 150
+#define Rpm_Max       146     /* 最大转速  rpm */
+
+
+//#define MOTOR_GO HAL_GPIO_WritePin(GPIOA, AIN1_Pin, GPIO_PIN_RESET); HAL_GPIO_WritePin(GPIOB, AIN2_Pin, GPIO_PIN_SET)
+//void Motor_Init(void);
+void Motor1_SetSpeed(int Speed);
+void Motor2_SetSpeed(int Speed);
+void Motor3_SetSpeed(int Speed);
+void Motor4_SetSpeed(int Speed);
+
+#endif /* MOTOR_H_ */
+
---
+++ MDK-ARM/pid.h
@@ -0,0 +1,28 @@
+#ifndef __PID_H_
+#define __PID_H_
+#include <main.h>
+
+int Incremental_PID_Left(int reality,int target,int reset);		//左轮 增量式PID
+int Incremental_PID_Right(int reality,int target);	    //右轮 增量式PID
+
+//左右轮都是单独的PID计算，不然static定义的变量会混叠，控制逻辑有问题
+long Position_PID_Left(long reality,long target,int reset);		//左轮 位置式PID
+long Position_PID_Right(long reality, long target);		//右轮 位置式PID
+
+
+void Reset_Position_PID_Left(void);
+void Reset_Incremental_PID_Left(void);
+
+// 系统控制接口
+void PID_System_Reset(void);
+
+
+
+
+// 声明全局变量
+extern long Integral_bias_Left;
+extern long Integral_bias_Right;
+extern float Bias_Left, Last_bias_Left, Prev_bias_Left;
+extern float Bias_Right, Last_bias_Right, Prev_bias_Right;
+static long Bias, Last_Bias; 
+#endif
